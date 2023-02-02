0a842ed59549149c01d4ff29ff2f096eb3d5ef3c
andrei_solovev_2 <sir.noen@yandex.ru>
2023-02-02 13:50:25 +0300

    add productitem and also productlist

---
+++ src/components/ProductList.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul class="catalog__list">
+        <ProductItem 
+        v-for="(product, idx) in products"
+        :key="idx"
+        :product="product"
+        ></ProductItem>
+    </ul>
+
+</template>
+
+<script>
+import ProductItem from "@/components/ProductItem";
+
+export default {
+    components: {ProductItem},
+    props: ['products']
+}
+
+</script>
+
+
+<style scoped>
+    
+</style>
